9a8fa1d84ebd7b825a1aaff742410cd6fd2e7bd8
HUARONG YUAN <2401713@uad.ac.uk>
2024-10-01 16:38:43 +0100

    Make a choice
    Player can make a simple choice now

--- The Scandal in Ink Project/Assets/Ink Scripts/Test.ink
+++ The Scandal in Ink Project/Assets/Ink Scripts/Test.ink
@@ -1,2 +1,17 @@
+-> main
+
+===main===
 Hello World!
 Hello You!
+Which one do you choose?
+    +[One]
+        ->chosen("One")
+    +[Two]
+        ->chosen("Two")
+    +[Three]
+        ->chosen("Three")
+
+===chosen(i)===
+You chose {i}!
+
+-> END
--- The Scandal in Ink Project/Assets/Scripts/Dialogue/DialogueManager.cs
+++ The Scandal in Ink Project/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -4,6 +4,7 @@ using TMPro;
 using UnityEngine;
 using Ink.Runtime;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -12,6 +13,12 @@ public class DialogueManager : MonoBehaviour
     [Header("Dialogue UI")]
     [SerializeField] private GameObject dialoguePanel;
     [SerializeField] private TextMeshProUGUI dialogueText;
+
+    [Header("Choices UI")]
+    // any number of choices
+    [SerializeField] private GameObject[] choices;
+    private TextMeshProUGUI[] choicesText;
+
 
     private Story currentStory;
 
@@ -44,6 +51,15 @@ public class DialogueManager : MonoBehaviour
 
         // continue the story if player click the continue button
         continueButton.onClick.AddListener(ContinueButtonClick);
+
+        // get all of the choices text
+        choicesText = new TextMeshProUGUI[choices.Length];
+        int index = 0;
+        foreach (GameObject choice in choices)
+        {
+            choicesText[index] = choice.GetComponentInChildren<TextMeshProUGUI>();
+            index++;
+        }
     }
 
     private void Update()
@@ -87,7 +103,11 @@ public class DialogueManager : MonoBehaviour
     {
         if (currentStory.canContinue)
         {
+            // set text for the current dialogue line
             dialogueText.text = currentStory.Continue();
+            // display choices, if any, for this dialogue line
+            if(currentStory.currentChoices != null)
+            DisplayChoices();
         }
         else
         {
@@ -98,7 +118,50 @@ public class DialogueManager : MonoBehaviour
     void ContinueButtonClick()
     {
         ContinueStory();
-    }    
+    }
+
+    private void DisplayChoices()
+    {
+        List<Choice> currentChoices = currentStory.currentChoices;
+
+        Debug.Log(currentChoices.Count);
+
+        if (currentChoices.Count > choices.Length)
+        {
+            Debug.LogError("More choices were given than UI can support. Number of choices give: "+currentChoices.Count);
+        }
+
+        int index = 0;
+        // enable and initialize the choices up to the amount of choices for this line of dialogue
+        foreach (Choice choice in currentChoices)
+        {
+            choices[index].gameObject.SetActive(true);
+            choicesText[index].text = choice.text;
+            index++;
+        }
+
+        // go through the remaining choices the UI supports and make sure they're hidden
+        for(int i = index; i < choices.Length; i++)
+        {
+            choices[i].gameObject.SetActive(false);
+        }
+
+        //StartCoroutine(SelectFirstChoice());
+    }
+
+    /*private IEnumerator SelectFirstChoice()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        yield return new WaitForEndOfFrame();
+        EventSystem.current.SetSelectedGameObject(choices[0].gameObject);
+    }*/
+
+    public void MakeChoice(int choiceIndex)
+    {
+        currentStory.ChooseChoiceIndex(choiceIndex);
+        //Debug.Log(currentStory.currentText);
+        ContinueStory();
+    }
 
 }
 
