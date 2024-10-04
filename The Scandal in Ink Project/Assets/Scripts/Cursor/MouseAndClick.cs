c9975a146bf0de6b03f1c303c711294f5e3eaa8f
HUARONG YUAN <2401713@uad.ac.uk>
2024-10-04 14:40:18 +0100

    Trasition
    Different scenes simply transition
    Set new Cursor manager

---
+++ The Scandal in Ink Project/Assets/Scripts/Cursor/MouseAndClick.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseAndClick : MonoBehaviour
+{
+    public Vector3 mouseWorldPos => Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+
+    private bool canClick;
+
+    // Update is called once per frame
+    void Update()
+    {
+        canClick = ObjectAtMousePosition();
+
+        if(DialogueManager.GetInstance().dialogueIsPlaying)
+        {
+            return;
+        }
+
+        if (canClick && Input.GetMouseButtonDown(0))
+        {
+            // identify the situations of mouse interaction
+
+            ClickAction(ObjectAtMousePosition().gameObject);
+
+
+            /*Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit;
+
+            if (hit = Physics2D.Raycast(ray.origin,ray.direction,Mathf.Infinity))
+            {
+                if (hit.collider != null)
+                {
+                    //Debug.Log(hit.collider.gameObject);
+                    hit.transform.GetComponent<DialogueTrigger>().isClicked = true;
+                }
+
+            }*/
+        }
+    }
+
+    private void ClickAction(GameObject clickObject)
+    {
+        switch (clickObject.tag)
+        {
+            case "Teleport":
+                var teleport = clickObject.GetComponent<Teleport>();
+                teleport?.TeleportToScene(); // check if it is null
+                break;
+            case "NPC":
+                clickObject.GetComponent<DialogueTrigger>().isClicked = true;
+                break;
+        }
+    }
+
+    /// <summary>
+    /// get the collider within mouse click position
+    /// </summary>
+    /// <returns></returns>
+
+    private Collider2D ObjectAtMousePosition()
+    {
+        return Physics2D.OverlapPoint(mouseWorldPos);
+    }
+
+
+}
