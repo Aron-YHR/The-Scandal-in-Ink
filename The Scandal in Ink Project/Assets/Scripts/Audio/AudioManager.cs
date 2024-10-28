3f04b89d2098371d7ee061f8ad537fe40aa41ca3
HUARONG YUAN <2401713@uad.ac.uk>
2024-10-28 16:58:07 +0000

    Basic volume settings
    TODO: set the UI to default volume

--- The Scandal in Ink Project/Assets/Scripts/Audio/AudioManager.cs
+++ The Scandal in Ink Project/Assets/Scripts/Audio/AudioManager.cs
@@ -1,22 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioManager : Singleton<AudioManager>
 {
     public AudioSource BGMSource;
     public AudioSource FXSource;
 
+    public AudioMixer audioMixer;
+
+    public Slider masterVolumeSlider;
+    public Slider bgmVolumeSlider;
+    public Slider fxVolumeSlider;
+
     private void OnEnable()
     {
         EventHandler.PlayFXAudioEvent += OnFXEvent;
         EventHandler.PlayBGMAudioEvent += OnBGMEvent;
+        //EventHandler.SetSliderVolumeEvent += OnSetSliderEvent;
+
+        masterVolumeSlider.onValueChanged.AddListener(OnMainVolumeChangeEvent);
+        bgmVolumeSlider.onValueChanged.AddListener(OnBGMVolumeChangeEvent);
+        fxVolumeSlider.onValueChanged.AddListener(OnFXVolumeChangeEvent);
     }
 
     private void OnDisable()
     {
         EventHandler.PlayFXAudioEvent -= OnFXEvent;
         EventHandler.PlayBGMAudioEvent -= OnBGMEvent;
+        //EventHandler.SetSliderVolumeEvent -= OnSetSliderEvent;
+        //EventHandler.ChangeVolumeEvent -= OnVolumeChangeEvent;
+        masterVolumeSlider.onValueChanged.RemoveListener(OnMainVolumeChangeEvent);
+        bgmVolumeSlider.onValueChanged.RemoveListener(OnBGMVolumeChangeEvent);
+        fxVolumeSlider.onValueChanged.RemoveListener(OnFXVolumeChangeEvent);
     }
 
     public void OnFXEvent(AudioClip audioClip)
@@ -29,5 +47,34 @@ public class AudioManager : Singleton<AudioManager>
     {
         BGMSource.clip = audioClip;
         BGMSource.Play();
+    }
+
+    public void OnMainVolumeChangeEvent(float amount)
+    {
+        audioMixer.SetFloat("MasterVolume", amount*100-80);
+        //EventHandler.CallPlayChangeVolumeEvent(amount);
+    }
+
+    public void OnBGMVolumeChangeEvent(float amount)
+    {
+        audioMixer.SetFloat("BGMVolume", amount * 100 - 80);
+        //EventHandler.CallPlayChangeVolumeEvent(amount);
+    }
+
+    public void OnFXVolumeChangeEvent(float amount)
+    {
+        audioMixer.SetFloat("FXVolume", amount * 100 - 80);
+        //EventHandler.CallPlayChangeVolumeEvent(amount);
+    }
+
+    public void OnSetSliderEvent()
+    {
+        float amount;
+        audioMixer.GetFloat("MasterVolume", out amount);
+        masterVolumeSlider.value = (amount+80) *100 ;
+        audioMixer.GetFloat("BGMVolume", out amount);
+        bgmVolumeSlider.value = (amount + 80) * 100;
+        audioMixer.GetFloat("FXVolume", out amount);
+        fxVolumeSlider.value = (amount + 80) * 100;
     }
 }
