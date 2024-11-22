243e8840aa139ffa5b2025c480172d5e2c69f72d
HUARONG YUAN <2401713@uad.ac.uk>
2024-11-22 12:03:17 +0000

    Update
    fix highlight show up when doing dialogue
    Change the mechanic for changing the font of dialogue

--- The Scandal in Ink Project/Assets/Scripts/Camera/CameraFollowMouse.cs
+++ The Scandal in Ink Project/Assets/Scripts/Camera/CameraFollowMouse.cs
@@ -1,10 +1,4 @@
 using Cinemachine;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Mathematics;
-using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
 
 public class CameraFollowMouse : Singleton<CameraFollowMouse>
@@ -37,7 +31,7 @@ public class CameraFollowMouse : Singleton<CameraFollowMouse>
     {
        
         //Debug.Log(envir.sprite.texture.height);
-        if (canMove && !DialogueManager.GetInstance().dialogueIsPlaying)
+        if (canMove && !DialogueManager.GetInstance().dialogueIsPlaying && !TransitionManager.Instance.isFading)
         {
             inputDir = new Vector3(0, 0, 0);
             if (Input.mousePosition.x < edgeScrollSize && Input.mousePosition.x > 0) inputDir.x = -1f;
