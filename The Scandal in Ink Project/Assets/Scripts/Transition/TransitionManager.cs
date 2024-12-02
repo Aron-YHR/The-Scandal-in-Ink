a0eba4ac51b61c1b508da38d7ef871dbc3dc8b38
MARTIN TAYLOR <2001176@uad.ac.uk>
2024-12-02 11:13:02 +0000

    updated Transition script

--- The Scandal in Ink Project/Assets/Scripts/Transition/TransitionManager.cs
+++ The Scandal in Ink Project/Assets/Scripts/Transition/TransitionManager.cs
@@ -156,15 +156,17 @@ public class TransitionManager : Singleton<TransitionManager>
 
         SceneManager.SetActiveScene(newScene);
 
-        if (newScene != null && newScene.name == "LadyPocket")
+        if (newScene != null && newScene.name.Contains("Pocket"))
         {
-            MouseAndClick.Instance.hand.gameObject.SetActive(true);
-            MouseAndClick.Instance.isHandShowed = true;
+            Cursor.SetCursor(MouseAndClick.Instance.handOpen, Vector2.zero, CursorMode.ForceSoftware);
+            //MouseAndClick.Instance.hand.gameObject.SetActive(true);
+            //MouseAndClick.Instance.isHandShowed = true;
         }
         else
         {
-            MouseAndClick.Instance.isHandShowed = false;
-            MouseAndClick.Instance.hand.gameObject.SetActive(false);
+            Cursor.SetCursor(cursorImg, new Vector2(0, 100), CursorMode.ForceSoftware);
+            //MouseAndClick.Instance.isHandShowed = false;
+            //MouseAndClick.Instance.hand.gameObject.SetActive(false);
         }
 
         // find background in a new scene
